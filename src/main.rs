4fbe33444877c075f4830db531197aad6466169f
telkomdev-jefri <jefdimar@gmail.com>
2023-03-08 19:31:11 +0700

    adding service get: user; to app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder, web::Data};
-use api::user::{create_user};
+use actix_web::{get, web::Data, App, HttpResponse, HttpServer, Responder};
+use api::user::{create_user, get_user};
 use repository::mongodb::MongoRepo;
 
 #[get("/")]
@@ -15,11 +15,14 @@ mod repository;
 async fn main() -> std::io::Result<()> {
     let db = MongoRepo::init().await;
     let db_data = Data::new(db);
-    HttpServer::new(move || App::new()
-        .app_data(db_data.clone())
-        .service(hello)
-        .service(create_user))
-        .bind(("localhost", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(db_data.clone())
+            .service(hello)
+            .service(create_user)
+            .service(get_user)
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
